65f0c8cf2186635ce31d1ff1a82e9e989f40aa1e
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2024-01-18 15:50:55 -0300

    iniciando curso

---
+++ cursoTypeScript.ts
@@ -0,0 +1,24 @@
+//let persona = "Miguel"
+//persona = 2
+
+// const persona = {
+//     name: "pepe",
+//     age: 30
+// }
+
+// persona.asd (inferencia, )
+
+// tipos basicos en typeScript 
+
+// const number = 1
+// const n:number = 2
+
+// let a = "hola"
+// let aaa = null
+// let b: undefined = undefined
+
+//let a  (al no asignarle un valor pasa a tener el tipo any)
+
+//let anyValue: any = "hola" (le estamos diciendo que ignore el tipo de typescript, si yo lo pongo un string no lo trates como un string).
+
+// let anyValue: unKnown = "hola" (le decimos que no sabemos que tipo de datos es)
